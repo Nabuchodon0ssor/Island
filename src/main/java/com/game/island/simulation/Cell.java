00cac01a138179b30c15e79c29982ac44bc79eff
Dimitri Pasunkov <javarusher01@gmail.com>
2025-08-30 18:33:28 +0200

    Created class Cell (for multithreading possible)

--- src/main/java/com/game/island/simulation/Cell.java
+++ src/main/java/com/game/island/simulation/Cell.java
@@ -1,4 +1,54 @@
 package com.game.island.simulation;
 
+import com.game.island.entities.Organism;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Cell {
+    private final int x;
+    private final int y;
+
+    private final Map<Class<? extends Organism>, List<Organism>> organisms = new ConcurrentHashMap<>();
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public synchronized void addOrganism(Organism organism) {
+        Class<? extends Organism> type = organism.getClass();
+        organisms.computeIfAbsent(type, k -> new ArrayList<>()).add(organism);
+    }
+
+    public synchronized void removeOrganism(Organism organism) {
+        Class<? extends Organism> type = organism.getClass();
+        List<Organism> list = organisms.get(type);
+        if (list != null) {
+            list.remove(organism);
+            if (list.isEmpty()) {
+                organisms.remove(type);
+            }
+        }
+    }
+
+    public List<Organism> getAllOrganisms() {
+        List<Organism> result = new ArrayList<>();
+        for (List<Organism> list : organisms.values()) {
+            synchronized (list) { // защита на случай, если другой поток меняет этот список
+                result.addAll(list);
+            }
+        }
+        return result;
+    }
+
+    public List<Organism> getOrganismsByType(Class<? extends Organism> type) {
+        return Collections.unmodifiableList(organisms.getOrDefault(type, Collections.emptyList()));
+    }
+
+    public boolean hasOrganismType(Class<? extends Organism> type) {
+        return organisms.containsKey(type);
+    }
 }
--- src/main/java/com/game/island/simulation/Island.java
+++ src/main/java/com/game/island/simulation/Island.java
@@ -1,4 +1,22 @@
 package com.game.island.simulation;
 
 public class Island {
+    private final int width;
+    private final int height;
+    private final Cell[][] cells;
+
+    public Island(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.cells = new Cell[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                cells[x][y] = new Cell(x, y);
+            }
+        }
+    }
+
+    public Cell getCell(int x, int y) {
+        return cells[x][y];
+    }
 }
